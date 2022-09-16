9c57f1cdb6224394590637e4c62211b24be90789
GallonKnight <tomomitomoe@gmail.com>
2022-09-16 15:44:19 +0800

    Modified Planner JS and CSS

--- src/Planner.js
+++ src/Planner.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+
 
 function Planner() {
   return (
-    <div>Planner Test Test</div>
+    <Container className='PlannerContainer'>
+      <h1>Planner Test Test</h1>
+      <p>
+        insert text here, prolly need some react component things
+      </p>
+
+      <div className="inexButton">
+        <Button variant="outline-secondary">Import</Button>{' '}
+        <Button variant="outline-secondary">Export</Button>{' '}
+      </div>
+
+    </Container>
   )
 }
 
