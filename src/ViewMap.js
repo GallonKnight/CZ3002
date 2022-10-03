c673fc9e0f0d5c7ddf798cdb5c3927ed4da65ba1
azrealthief <tzechuan_1998@hotmail.com>
2022-10-03 12:44:00 +0800

    Updated ViewMap.js and google map is working now

--- src/ViewMap.js
+++ src/ViewMap.js
@@ -1,109 +1,16 @@
-import React, { Component } from 'react';
-import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
-import PlacesAutocomplete, {
-  geocodeByAddress,
-  getLatLng,
-} from 'react-places-autocomplete';
+// App.js
+import React from 'react';
+import './index.css';
+import MyGoogleMap from './components/MyGoogleMap';
 
-export class MapContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // for google map places autocomplete
-      address: '',
 
-      showingInfoWindow: false,
-      activeMarker: {},
-      selectedPlace: {},
-  
-      mapCenter: {
-        lat: 1.331232,
-        lng: 103.814020
-      }
-    };
-  }
+function ViewMap() {
 
-  handleChange = address => {
-    this.setState({ address });
-  };
- 
-  handleSelect = address => {
-    this.setState({ address });
-    geocodeByAddress(address)
-      .then(results => getLatLng(results[0]))
-      .then(latLng => {
-        console.log('Success', latLng);
-
-        // update center state
-        this.setState({ mapCenter: latLng });
-      })
-      .catch(error => console.error('Error', error));
-  };
- 
-  render() {
-    return (
-      <div id='googleMaps'>
-        <PlacesAutocomplete
-          value={this.state.address}
-          onChange={this.handleChange}
-          onSelect={this.handleSelect}
-        >
-          {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-            <div>
-              <input
-                {...getInputProps({
-                  placeholder: 'Search Places ...',
-                  className: 'location-search-input',
-                })}
-              />
-              <div className="autocomplete-dropdown-container">
-                {loading && <div>Loading...</div>}
-                {suggestions.map(suggestion => {
-                  const className = suggestion.active
-                    ? 'suggestion-item--active'
-                    : 'suggestion-item';
-                  // inline style for demonstration purpose
-                  const style = suggestion.active
-                    ? { backgroundColor: '#fafafa', cursor: 'pointer' }
-                    : { backgroundColor: '#ffffff', cursor: 'pointer' };
-                  return (
-                    <div
-                      {...getSuggestionItemProps(suggestion, {
-                        className,
-                        style,
-                      })}
-                    >
-                      <span>{suggestion.description}</span>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          )}
-        </PlacesAutocomplete>
-
-        <Map 
-          google={this.props.google}
-          initialCenter={{
-            lat: this.state.mapCenter.lat,
-            lng: this.state.mapCenter.lng
-          }}
-          center={{
-            lat: this.state.mapCenter.lat,
-            lng: this.state.mapCenter.lng
-          }}
-        >
-          <Marker 
-            position={{
-              lat: this.state.mapCenter.lat,
-              lng: this.state.mapCenter.lng
-            }} />
-        </Map>
-      </div>
-    )
-  }
+  return (
+    <div className="main-wrapper">
+      <MyGoogleMap />
+    </div>
+  );
 }
 
-export default GoogleApiWrapper({
-  apiKey: ('AIzaSyCSPAvbepDjGB63R0bCopylFHtrr4wiIVs')
-})(MapContainer)
+export default ViewMap;
